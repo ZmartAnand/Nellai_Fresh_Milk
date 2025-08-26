e5f7d57b152a48e11994dec69e584f527d88456e
Anand <s.anandsiva7897@gmail.com>
2025-08-26 15:08:14 +0530

    updateD

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter } from "@angular/router";
+import { routes } from "./app.routes";
+import { provideFirebaseApp, initializeApp } from "@angular/fire/app";
+import { provideHttpClient } from "@angular/common/http";
+import { provideAuth, getAuth } from "@angular/fire/auth";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
 
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+const firebaseConfig = {
+  apiKey: "AIzaSyCy3gB6DA02-ilvLAXtVW9YqzzkoOcqxwk",
+  authDomain: "nellai-freshmilk.firebaseapp.com",
+  projectId: "nellai-freshmilk",
+  storageBucket: "nellai-freshmilk.firebasestorage.app",
+  messagingSenderId: "4970155922",
+  appId: "1:4970155922:web:ad9775b3b35eaf605cbb4b",
+  measurementId: "G-Y6KNHL4J1D"
 };
+
+export const appConfig = [
+  provideRouter(routes),
+  provideFirebaseApp(() => initializeApp(firebaseConfig)),
+  provideFirestore(() => getFirestore()),
+  provideHttpClient(),
+  provideAuth(() => getAuth()),
+];
