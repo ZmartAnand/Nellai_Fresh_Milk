78efc2ec9f1684d4d489f313132ed98662c72f69
Anand <s.anandsiva7897@gmail.com>
2025-08-28 11:30:51 +0530

    UpdateD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component,AfterViewInit  } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+
+
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-root',
@@ -9,4 +13,17 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'nellai-fresh-milk';
+  ngAfterViewInit() {
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+    const menuToggle = document.getElementById('navbarNav');
+    const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false });
+
+    navLinks.forEach((l: any) => {
+      l.addEventListener('click', () => {
+        if (menuToggle?.classList.contains('show')) {
+          bsCollapse.hide();
+        }
+      });
+    });
+  }
 }
